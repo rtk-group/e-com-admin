6b95520651a1ed6d80290d72a79c8075bf12026a
rtk-group <mydevclass9690@gmail.com>
2025-02-28 09:55:38 -0800

    create side bar & admin authenti

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import { Routes, Route } from 'react-router-dom';
+import Add from './pages/Add.jsx'
+import List from './pages/List.jsx'
+import Orders from './pages/Orders.jsx'
+import Login from './components/Login.jsx'
+
 
 const App = () =>{
+
+    const [token, settoken] = useState('');
+
+
+
+
     return(
-        <>
-          <Navbar/>  
+        <div className="bg-gray-50 min-screen">
+            {
+                token === ""? <Login/>:
+                <>
+          <Navbar/> 
+           <hr/>
+           <div className="flex w-full">
+            <Sidebar/>
+            <div className="w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base">
+                <Routes>
+                    <Route path='/add' element={<Add/>}/>
+                    <Route path='/list' element={<List/>}/>
+                    <Route path='/orders' element={<Orders/>}/>
+                </Routes>
+            </div>
+           </div>
         </>
+            }
+          
+        </div>
     )
 }
 
